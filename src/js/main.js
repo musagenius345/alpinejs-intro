e587b0685f9513eef73798d5387e2011743c6240
musagenius345 <musabadru@gmail.com>
2023-08-04 17:20:03 +0300

    Ui done

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+let template = `<article class="card flex-card">
+        <div class="img">
+          <img src="musa.jpg" alt="/musa.jpg" width="750" height="750">
+        </div>
+        <div class="info">
+          <h3 class="country" data-country-name="Germany">Germany</h3>
+          <p class="info__detail">Population: <span class="population" data-population="0"></span> </p>
+          <p class="info__detail">Region: <span class="region" data-region="region"></span></p>
+          <p class="info__detail">Capital: <span class="capital" data-capital=""></span></p>
+        </div>
+      </article>`
